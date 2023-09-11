2033f99c7aee506f5af18026a3cab1c93bd0439f
Aidan Gauland <aidalgol@fastmail.net>
2023-09-11 21:22:28 +1200

    Remove flake-utils
    Rationale: https://ayats.org/blog/no-flake-utils/

--- flake.nix
+++ flake.nix
@@ -1,68 +1,73 @@
 {
   description = "Nix fether for Steam games";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
-    flake-utils.url = "github:numtide/flake-utils";
-  };
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
 
   outputs = {
     self,
     nixpkgs,
-    flake-utils,
-  }:
-    with flake-utils.lib;
+  }: let
     # DepotDownloader only supports x86_64 Linux.
-      eachSystem ["x86_64-linux"] (system: let
-        pkgs = import nixpkgs {
-          inherit system;
-          config.allowUnfree = true;
-          overlays = [self.overlays.default];
-        };
-
-        linters = with pkgs; [
-          alejandra
-          statix
-          shellcheck
-          shfmt
-        ];
-      in {
-        devShells = {
-          default = pkgs.mkShell {
-            packages = with pkgs;
-              [
-                nil # Nix LS
-                nodePackages.bash-language-server
-              ]
-              ++ linters;
-          };
-        };
-
-        checks =
-          builtins.mapAttrs (name: pkgs.runCommandLocal name {nativeBuildInputs = linters;}) {
-            alejandra = "alejandra --check ${./.} > $out";
-            shellcheck = "shellcheck $(${pkgs.shfmt}/bin/shfmt --find ${./.}) > $out";
-            shfmt = "shfmt --simplify --diff ${./.} > $out";
-            statix = "statix check ${./.} > $out";
-          }
-          // {
-            inherit (pkgs) steamworks-sdk-redist;
-          };
-
-        formatter = pkgs.writeShellApplication {
-          name = "fmt";
-          runtimeInputs = linters;
-          text = ''
-            alejandra --quiet .
-            statix fix .
-            shfmt --simplify --write .
-          '';
-        };
-      })
-      // {
-        overlays.default = final: prev: {
-          fetchSteam = final.callPackage ./fetch-steam {};
-          steamworks-sdk-redist = final.callPackage ./steamworks-sdk-redist {};
-        };
+    supportedSystems = ["x86_64-linux"];
+    forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    pkgsFor = system:
+      import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+        overlays = [self.overlays.default];
       };
+    lintersFor = system: let
+      pkgs = pkgsFor system;
+    in
+      with pkgs; [
+        alejandra
+        statix
+        shellcheck
+        shfmt
+      ];
+  in {
+    devShells = forAllSystems (system: let
+      pkgs = pkgsFor system;
+    in {
+      default = pkgs.mkShell {
+        packages = with pkgs;
+          [
+            nil # Nix LS
+            nodePackages.bash-language-server
+          ]
+          ++ (lintersFor system);
+      };
+    });
+
+    checks = forAllSystems (system: let
+      pkgs = pkgsFor system;
+    in
+      builtins.mapAttrs (name: pkgs.runCommandLocal name {nativeBuildInputs = lintersFor system;}) {
+        alejandra = "alejandra --check ${./.} > $out";
+        shellcheck = "shellcheck $(${pkgs.shfmt}/bin/shfmt --find ${./.}) > $out";
+        shfmt = "shfmt --simplify --diff ${./.} > $out";
+        statix = "statix check ${./.} > $out";
+      }
+      // {
+        inherit (pkgs) steamworks-sdk-redist;
+      });
+
+    formatter = forAllSystems (system: let
+      pkgs = pkgsFor system;
+    in
+      pkgs.writeShellApplication {
+        name = "fmt";
+        runtimeInputs = lintersFor system;
+        text = ''
+          alejandra --quiet .
+          statix fix .
+          shfmt --simplify --write .
+        '';
+      });
+
+    overlays.default = final: prev: {
+      fetchSteam = final.callPackage ./fetch-steam {};
+      steamworks-sdk-redist = final.callPackage ./steamworks-sdk-redist {};
+    };
+  };
 }
